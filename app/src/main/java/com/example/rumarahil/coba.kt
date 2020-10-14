229b693259730b01fb76881765add2c4c6a961c3
Rferdian516 <rferdian516@gmail.com>
2020-10-14 15:08:47 +0700

    edo login&register

---
+++ app/src/main/java/com/example/rumarahil/coba.kt
@@ -0,0 +1,13 @@
+package com.example.rumarahil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class coba : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coba)
+
+
+    }
+}
